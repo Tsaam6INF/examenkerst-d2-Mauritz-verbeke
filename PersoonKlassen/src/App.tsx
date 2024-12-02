4071826e729846981f603afbcc45113d4282d3c4
Mauritz Verbeke <Mauritz.Verbeke@student.tsaam.be>
2024-12-02 09:52:27 +0100

    Commentaar toegevoegd

--- PersoonKlassen/src/App.tsx
+++ PersoonKlassen/src/App.tsx
@@ -7,14 +7,18 @@ import { Fragment } from "react/jsx-runtime";
 import Persoon from "./components/persoon";
 
 function App() {
+  // Dit is de const dat ik mee gekregen heb voor mijn opdracht.
   const start = [
     { naam: "karel kleintjes", klassen: ["1A", "2A", "3STW"] },
     { naam: "Els Dotjes", klassen: ["1A", "2B", "3H"] },
+    { naam: "Jonas Kaas", klassen: ["1C", "5B", "2H"] },
   ];
 
   return (
     <Fragment>
+      {/* Dit is een lijst die aangemaakt word voor al je personen in te steken */}
       <ul className="list-group list-group-horizontal">
+        {/* Hier map je je start dat je mee gekregen hebt zoo dat het per persoon wordt gemaakt mijn app persoon. */}
         {start.map((data) => (
           <Persoon data={data} />
         ))}
--- PersoonKlassen/src/components/persoon.tsx
+++ PersoonKlassen/src/components/persoon.tsx
@@ -1,31 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-
+// dit is de interface voor de data dat je mee krijgt uit je app.tsx
 interface typeGData {
   naam: string;
   klassen: string[];
 }
-
+// dit is d einterface voor de typeData in een props te steken dit maakt het makelijker en moet je in je functie niet veel extra schrijven.
 interface props {
   data: typeGData;
 }
-
+// dit is de functie Persoon dat ook gerund wordt in je App.tsx met daarin je props met data.
 function Persoon({ data }: props) {
-  const [namen] = useState(data.naam);
+  // hier useState gebruikt om je data uit je props te halen naar naam en klassen zoo dat je die kan gebruiken in je functie.
+  const [naam] = useState(data.naam);
   const [klassen] = useState(data.klassen);
-
+  // dit is je return van je functie
   return (
     <>
-      <li className="list-group-item">
-        <h1>Naam: {namen}</h1>
+      {/* hier maak ik een div aan waar al je data in komt vna je naam en klassen */}
+      <div className="list-group-item">
+        {/* hier steek ik de naam er in door het te laten renderen */}
+        <h1>Naam : {naam}</h1>
+        {/* hier maak ik een list waar al je klassen in gaan komen door gebruik te maken van map die alle klassen in klassen zal appart paken en er zal insteken. */}
         <ul className="list-group list-group-horizontal">
           {klassen.map((klas) => (
-            <li className="list-group-item list-group-item-action list-group-item-dark">
+            <li className="list-group-item list-group-item-action list-group-item-dark text-center">
+              {/* Hier wordt je klas er ingestoken */}
               {klas}
             </li>
           ))}
         </ul>
-      </li>
+      </div>
     </>
   );
 }
